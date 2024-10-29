99d51978ec230890892b1ecf1dacc055dd37c952
hugo926 <hugoclaveria99@gmail.com>
2024-10-29 09:49:12 +0100

    funcion suma

--- Reto1/src/retos/funciones1.java
+++ Reto1/src/retos/funciones1.java
@@ -4,7 +4,15 @@ public class funciones1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
 	}
+	
+	
+	public static int suma(int num1, int num2)
 
+	{
+		return num1+num2;
+	}
+	
 }
