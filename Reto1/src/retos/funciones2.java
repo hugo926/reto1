dd789a5c60411c3fe8d2a9187ad2d9580dea5568
Samu-2 <samuel.m27a@gmail.com>
2024-10-29 09:51:25 +0100

    funcion dividir

--- Reto1/src/retos/funciones2.java
+++ Reto1/src/retos/funciones2.java
@@ -11,5 +11,10 @@ public static int multiplicar(int numero1,int numero2) {
 	 return numero1*numero2;
 	
 }
-
+public static int dividir(int numero1,int numero2) {
+	int resultado=0;
+	if(numero2>0 && numero1>0)
+		 resultado= numero1/numero2;
+	return resultado;
+}
 }
